128b2f68bd6e548e1a2af23dfab4b80f89ee8297
CHAO <chao@CHAOdeMac-mini.local>
2014-09-02 10:07:50 +0800

    Initial Commit

---
+++ RequestTest1/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RequestTest1
+//
+//  Created by CHAO on 14-9-2.
+//  Copyright (c) 2014年 CHAO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
